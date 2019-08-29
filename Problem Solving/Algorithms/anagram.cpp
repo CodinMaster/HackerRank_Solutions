5572136e073f5ddf6546e0aec64f61e9330011fd
CodinMaster <latheyapoorv@gmail.com>
2019-08-29 14:26:39 +0530

    added new soln: anagram

---
+++ Problem Solving/Algorithms/anagram.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    string s[t];
+    int ans[t];
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> s[i];
+        ans[i] = 0;
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        if (s[i].length() % 2 != 0)
+        {
+            ans[i] = -1;
+            continue;
+        }
+
+        string half1 = s[i].substr(0, s[i].length() / 2);
+        string half2 = s[i].substr(s[i].length() / 2);
+
+        sort(half1.begin(), half1.end());
+        sort(half2.begin(), half2.end());
+
+        int count = 0;
+        for (int k = 0; k < s[i].length() / 2; k++)
+        {
+            int count_t = count;
+            for (int l = 0; l < s[i].length() / 2; l++)
+            {
+                if (half1[k] == half2[l])
+                {
+                    half2[l] = 0;
+                    count++;
+                    break;
+                }
+            }
+            if (count_t == count)
+            {
+                ans[i]++;
+            }
+        }
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        cout << ans[i] << "\n";
+    }
+
+    return 0;
+}
+#include <bits/stdc++.h>
